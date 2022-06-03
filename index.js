ba1f416d8fb1d898bd2f6add84405d2faf6ec269
Kalyan-Singh <99746258+Kalyan-Singh@users.noreply.github.com>
2022-06-03 21:48:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,156 @@
-const n=document.querySelectorAll("button").length;
+document.addEventListener("keypress",function_list);
 
-for (var i=0;i<n;i++){
-    var ele=document.querySelectorAll("button")[i];
-    switch(i){
-        case 0:
-            ele.addEventListener('keydown',myclick0);
-            //ele=this;
-            //this.
+var buttons = document.querySelectorAll("button");
+
+var n=buttons.length;
+
+for(let i=0;i<n;i++){
+    buttons[i].addEventListener("click",playOnClick);
+}
+function playOnClick(event){
+    let tmp= event.target.innerHTML;
+        switch(tmp){
+            case "w":
+                var aud=new Audio("sounds/tom-1.mp3");
+                aud.play();
+                document.querySelector(".w").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".w").classList.remove("pressed");
+                },100);
+                break;
+            case "a":
+                var aud=new Audio("sounds/tom-2.mp3");
+                aud.play();
+                document.querySelector(".a").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".a").classList.remove("pressed");
+                },100);
+                break;
+            case "s":
+                var aud=new Audio("sounds/tom-3.mp3");
+                aud.play();
+                document.querySelector(".s").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".s").classList.remove("pressed");
+                },100);
+                break;
+            case "d":
+                var aud=new Audio("sounds/tom-4.mp3");
+                aud.play();
+                document.querySelector(".d").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".d").classList.remove("pressed");
+                },100);
+                break;
+            case "j":
+                var aud=new Audio("sounds/snare.mp3");
+                aud.play();
+                document.querySelector(".j").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".j").classList.remove("pressed");
+                },100);
+                break;
+            case "k":
+                var aud=new Audio("sounds/crash.mp3");
+                aud.play();
+                document.querySelector(".k").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".k").classList.remove("pressed");
+                },100);
+                break;
+            case "l":
+                var aud=new Audio("sounds/kick-bass.mp3");
+                aud.play();
+                document.querySelector(".l").classList.add("pressed");
+                setTimeout(function(){
+                    document.querySelector(".l").classList.remove("pressed");
+                },100);
+                break;
+        }
+}
+
+
+function function_list(event){
+    soundPlayer(event);
+    animator(event);
+}
+
+function soundPlayer(event){
+    switch(event.key){
+        case "w":
+            var aud=new Audio("sounds/tom-1.mp3");
+            aud.play();
             break;
-        case 1:
-            ele.addEventListener("click",myclick1);
+        case "a":
+            var aud=new Audio("sounds/tom-2.mp3");
+            aud.play();
             break;
-        case 2:
-            ele.addEventListener("click",myclick2);
+        case "s":
+            var aud=new Audio("sounds/tom-3.mp3");
+            aud.play();
             break;
-        case 3:
-            ele.addEventListener("click",myclick3);
+        case "d":
+            var aud=new Audio("sounds/tom-4.mp3");
+            aud.play();
             break;
-        case 4:
-            ele.addEventListener("click",myclick4);
+        case "j":
+            var aud=new Audio("sounds/snare.mp3");
+            aud.play();
             break;
-        case 5:
-            ele.addEventListener("click",myclick5);
+        case "k":
+            var aud=new Audio("sounds/crash.mp3");
+            aud.play();
             break;
-        case 6:
-            ele.addEventListener("click",myclick6);
-            break;
+        case "l":
+            var aud=new Audio("sounds/kick-bass.mp3");
+            aud.play();
+            break;    
     }
 }
-
-
-function myclick0(){
-    //const keyname=myclick0.key;
-    //if (keyname==='w'){
-    var aud=new Audio("sounds/tom-1.mp3");
-    aud.play();//}
-}
-
-function myclick1(){
-    var aud=new Audio("sounds/tom-2.mp3");
-    aud.play();
-}
-function myclick2(){
-    var aud=new Audio("sounds/tom-3.mp3");
-    aud.play();
-}
-function myclick3(){
-    var aud=new Audio("sounds/tom-4.mp3");
-    aud.play();
-}
-function myclick4(){
-    var aud=new Audio("sounds/snare.mp3");
-    aud.play();
-}
-function myclick5(){
-    var aud=new Audio("sounds/crash.mp3");
-    aud.play();
-}
-function myclick6(){
-    var aud=new Audio("sounds/kick-bass.mp3");
-    aud.play();
-}
+function animator(event){
+    
+    switch(event.key){
+        case "w":
+            document.querySelector(".w").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".w").classList.remove("pressed");
+            },100);
+            break;
+        case "a":
+            document.querySelector(".a").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".a").classList.remove("pressed");
+            },100);
+            break;
+        case "s":
+            document.querySelector(".s").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".s").classList.remove("pressed");
+            },100);
+            break;
+        case "d":
+            document.querySelector(".d").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".d").classList.remove("pressed");
+            },100);
+            break;
+        case "j":
+            document.querySelector(".j").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".j").classList.remove("pressed");
+            },100);
+            break;
+        case "k":
+            document.querySelector(".k").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".k").classList.remove("pressed");
+            },100);
+            break;
+        case "l":
+            document.querySelector(".l").classList.add("pressed");
+            setTimeout(function(){
+                document.querySelector(".l").classList.remove("pressed");
+            },100);
+            break;
+        }
+    }
